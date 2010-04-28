e1911ca56ea02ec1f089b683a7019f13c79feeff
Alexey 'l1feh4ck3r' Antonov <1ifeh4ck3r@gmail.com>
2010-04-28 11:15:25 +1100

    Code formatting.

--- example/clxx_example/vector_add.cpp
+++ example/clxx_example/vector_add.cpp
@@ -7,43 +7,43 @@ using namespace CLxx;
 
 
 const char* code = 
-"__kernel void VectorAdd(__global const float* a, __global const float* b, __global float* c, int iNumElements)"
-"{"
-"    int iGID = get_global_id(0);"           // get index into global data array
-"    if (iGID >= iNumElements) { return; }"  // bound check (equivalent to the limit on a 'for' loop for standard/serial C code
-"    c[iGID] = a[iGID] + b[iGID];"           // add the vector elements
-"}";
+        "__kernel void VectorAdd(__global const float* a, __global const float* b, __global float* c, int iNumElements)"
+        "{"
+        "    int iGID = get_global_id(0);"           // get index into global data array
+        "    if (iGID >= iNumElements) { return; }"  // bound check (equivalent to the limit on a 'for' loop for standard/serial C code
+        "    c[iGID] = a[iGID] + b[iGID];"           // add the vector elements
+        "}";
 
 
 
 int main(int argc, char * argv[])
 {
- try
- {
-
-    // init data
-
-    const int numElements = 114447;	
-    const size_t localWorkSize = 256;
-    const int r = numElements % localWorkSize;
-
-    const size_t globalGroupSize = r == 0 ? numElements : numElements + localWorkSize - r;
-
-    std::vector<float> vecA(globalGroupSize);
-    std::vector<float> vecB(globalGroupSize); 
-    std::vector<float> vecR(globalGroupSize);  // result
-
-    const float fScale = 1.0f / (float)RAND_MAX;
-    for (unsigned i = 0; i < globalGroupSize; ++i) 
+    try
     {
-        vecA[i] = fScale * rand();
-		vecB[i] = fScale * rand();
-    }
+
+        // init data
+
+        const int numElements = 114447;
+        const size_t localWorkSize = 256;
+        const int r = numElements % localWorkSize;
+
+        const size_t globalGroupSize = r == 0 ? numElements : numElements + localWorkSize - r;
+
+        std::vector<float> vecA(globalGroupSize);
+        std::vector<float> vecB(globalGroupSize);
+        std::vector<float> vecR(globalGroupSize);  // result
+
+        const float fScale = 1.0f / (float)RAND_MAX;
+        for (unsigned i = 0; i < globalGroupSize; ++i)
+        {
+            vecA[i] = fScale * rand();
+            vecB[i] = fScale * rand();
+        }
 
 
-    // Steps :
+        // Steps :
 
-    // 1) create context
+        // 1) create context
 	boost::shared_ptr<Context> ctx = Context::createContext(Device::GPU); 
 
 	// 2) create a program using source code
@@ -52,12 +52,12 @@ int main(int argc, char * argv[])
 	// 3) compile
 	if ( !prog->buildProgram("-cl-mad-enable") )
 	{
-        std::cout << prog->getBuildLog() << std::endl;
-        return 1;
+            std::cout << prog->getBuildLog() << std::endl;
+            return 1;
 	}
 
 
-    // 4) allocate memory on the device to hold the vectors data
+        // 4) allocate memory on the device to hold the vectors data
    	boost::shared_ptr<Buffer>  vecBufferA =  ctx->createBuffer(Memory::READ_ONLY, sizeof(float) * globalGroupSize);
 	boost::shared_ptr<Buffer>  vecBufferB =  ctx->createBuffer(Memory::READ_ONLY, sizeof(float) * globalGroupSize);
 	boost::shared_ptr<Buffer>  verBufferR =  ctx->createBuffer(Memory::WRITE_ONLY, sizeof(float) * globalGroupSize);
@@ -65,16 +65,16 @@ int main(int argc, char * argv[])
 	// 5) create the commands to copy the data to and from the device
 	boost::shared_ptr<WriteBufferCommand> wrtBufferA( new WriteBufferCommand(vecBufferA,&vecA[0]) );
 	boost::shared_ptr<WriteBufferCommand> wrtBufferB( new WriteBufferCommand(vecBufferB,&vecB[0]) );
-    boost::shared_ptr<ReadBufferCommand>  readBuffer( new ReadBufferCommand (verBufferR,&vecR[0]) );
+        boost::shared_ptr<ReadBufferCommand>  readBuffer( new ReadBufferCommand (verBufferR,&vecR[0]) );
 
 	// 6) create the kernel to use and set the arguments 
 	boost::shared_ptr<Kernel> kernel = prog->createKernel("VectorAdd");
 	kernel->setArguments(vecBufferA,vecBufferB,verBufferR,numElements);
 
 
-    // 7) create the command to execute the kernel
-    boost::shared_ptr<Range1DCommand> calculateOnDataRange( new Range1DCommand(kernel,globalGroupSize,localWorkSize) );
-    
+        // 7) create the command to execute the kernel
+        boost::shared_ptr<Range1DCommand> calculateOnDataRange( new Range1DCommand(kernel,globalGroupSize,localWorkSize) );
+
 	// 8) create a command queue
 	boost::shared_ptr<CommandQueue> cmdQueue = ctx->createCommandQueue();
 
@@ -84,12 +84,12 @@ int main(int argc, char * argv[])
 	cmdQueue->enque(calculateOnDataRange); // execute kernel 
 	cmdQueue->enque(readBuffer); // read the results
 
-	}
-	catch(Exception ex)
-	{
+    }
+    catch(Exception ex)
+    {
         std::cout << ex.what() << std::endl;
         return 1;
-	}
+    }
     
     return 0;
 }
